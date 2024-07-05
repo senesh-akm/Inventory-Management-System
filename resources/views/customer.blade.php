60417235a8957a4f1df9d09280b51130be082a24
Ranusha Senesh Akmeemana <seneshak2001@gmail.com>
2024-07-05 21:10:14 +0530

    Created customer funtions, regenerated customer table, edited blades and added routes.

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -5,46 +5,47 @@
 @section('content')
     <main class="mt-5">
         <h2>Add Customer</h2>
-        <form>
+        <form action="{{ route('customers.store') }}" method="POST">
+            @csrf
             <div class="form-group">
                 <label for="customerCode">Customer Code</label>
-                <input type="text" class="form-control" id="customerCode" placeholder="Enter Customer Code">
+                <input type="text" class="form-control" id="CustomerCode" name="CustomerCode" placeholder="Enter Customer Code" required>
             </div>
             <div class="form-group">
                 <label for="customer">Customer</label>
-                <input type="text" class="form-control" id="customer" placeholder="Enter Customer">
+                <input type="text" class="form-control" id="Customer" name="Customer" placeholder="Enter Customer" required>
             </div>
             <div class="form-group">
                 <label for="contactTitle">Contact Title</label>
-                <input type="text" class="form-control" id="contactTitle" placeholder="Enter Contact Title">
+                <input type="text" class="form-control" id="ContactTitle" name="ContactTitle" placeholder="Enter Contact Title" required>
             </div>
             <div class="form-group">
                 <label for="contactName">Contact Name</label>
-                <input type="text" class="form-control" id="contactName" placeholder="Enter Contact Name">
+                <input type="text" class="form-control" id="ContactName" name="ContactName" placeholder="Enter Contact Name" required>
             </div>
             <div class="form-group">
                 <label for="address">Address</label>
-                <input type="text" class="form-control" id="address" placeholder="Enter Address">
+                <input type="text" class="form-control" id="Address" name="Address" placeholder="Enter Address" required>
             </div>
             <div class="form-group">
                 <label for="city">City</label>
-                <input type="text" class="form-control" id="city" placeholder="Enter City">
+                <input type="text" class="form-control" id="City" name="City" placeholder="Enter City" required>
             </div>
             <div class="form-group">
                 <label for="postalCode">Postal Code</label>
-                <input type="text" class="form-control" id="postalCode" placeholder="Enter Postal Code">
+                <input type="text" class="form-control" id="PostalCode" name="PostalCode" placeholder="Enter Postal Code" required>
             </div>
             <div class="form-group">
                 <label for="country">Country</label>
-                <input type="text" class="form-control" id="country" placeholder="Enter Country">
+                <input type="text" class="form-control" id="Country" name="Country" placeholder="Enter Country" required>
             </div>
             <div class="form-group">
                 <label for="phone">Phone</label>
-                <input type="text" class="form-control" id="phone" placeholder="Enter Phone Number">
+                <input type="text" class="form-control" id="Phone" name="Phone" placeholder="Enter Phone Number" required>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter Email">
+                <input type="email" class="form-control" id="Email" name="Email" placeholder="Enter Email" required>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
