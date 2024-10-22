e9163841cfd17e4bd46b8f6e0b755ec858e323ce
Senesh Akmeemana <seneshak2001@gmail.com>
2024-10-22 20:51:33 +0530

    Updated left-navbar, top-navbar, and app components

--- resources/views/partials/left-navbar.blade.php
+++ resources/views/partials/left-navbar.blade.php
@@ -1,33 +1,54 @@
-<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-    <div class="position-sticky">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Orders</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Products</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Customers</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Reports</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Integrations</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User</a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Profile</a>
-                    <a class="dropdown-item" href="#">Settings</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Logout</a>
-                </div>
-            </li>
-        </ul>
+{{-- Sidebar --}}
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sidebar">
+    <div class="container-fluid">
+        {{-- Toggle button for mobile view, positioned at top-left --}}
+        <button class="navbar-toggler position-absolute top-0 start-0 m-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav flex-column">
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5 bg-orange text-white active" aria-current="page" href="#" style="padding-right: 130px;">Dashboard</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5" href="#">Orders</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5" href="#">Products</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5" href="#">Customers</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5" href="#">Transaction</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5" href="#">Reports</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5" href="#">Integrations</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link fs-5" href="#">Users</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
+
+<script>
+    // Get all nav-links
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    // Add click event listener to each nav-link
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            // Remove 'active' class from all nav-links
+            navLinks.forEach(link => link.classList.remove('active', 'bg-orange', 'text-white'));
+
+            // Add 'active' class to the clicked nav-link
+            this.classList.add('active', 'bg-orange', 'text-white');
+        });
+    });
+</script>
--- resources/views/partials/top-navbar.blade.php
+++ resources/views/partials/top-navbar.blade.php
@@ -33,7 +33,7 @@
                 @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
+                            {{ Auth::user()->empname }}
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
