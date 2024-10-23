27598a935cad3b152842fd0c3881b648cad747d9
Senesh Akmeemana <seneshak2001@gmail.com>
2024-10-24 01:23:28 +0530

    Updated layouts

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     {{-- Link the Bootstrap Icons CDN --}}
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.0/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
 
     {{-- Link the compiled CSS and JS using Vite --}}
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
